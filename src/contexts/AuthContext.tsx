9edc224956e6435ddc6a185f43254fdd011b2a9e
freakact.js <62394230+tumulakivan@users.noreply.github.com>
2025-05-28 04:25:46 +0800

    ignore eslint

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -53,6 +53,7 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   );
 };
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useAuth = () => {
   const context = useContext(AuthContext);
   if (context === undefined) {
